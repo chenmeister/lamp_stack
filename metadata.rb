21bbdce857228d957d41a24c276cd4c5e400d65b
Sean Chen <sean.chen909@gmail.com>
2017-03-16 18:32:40 -0700

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'lamp_stack'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures lamp_stack'
+long_description 'Installs/Configures lamp_stack'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/lamp_stack/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/lamp_stack' if respond_to?(:source_url)
